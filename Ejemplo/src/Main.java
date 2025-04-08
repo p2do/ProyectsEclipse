1828a736791a56e94e3ef6c6df61fc4e5f433159
Antonio <antoniofs360@gmail.com>
2025-04-08 17:02:41 +0200

    Second commit

---
+++ Ejemplo/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+	
+			}
+			
+	public class Animal{
+		int edad;
+		int altura;
+		public Animal(int edad, int altura) {
+			
+		}
+	}
+}
+
+
+}
